b839e1582b86985f9194a9631ce0df8bde9176ac
Rasmus <rasmus.klaaser@voco.ee>
2023-11-01 14:28:21 +0200

    Added static css and image files

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.engine('hbs', hbs.engine({
     defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts/',
 }))
+//  setup static public directory
+app.use(express.static('public'))
+
 
 const mysql = require('mysql')
 
