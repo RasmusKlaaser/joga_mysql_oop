053aa983264a6b791b0fc6a3bc3253a142ccd760
Rasmus <rasmus.klaaser@voco.ee>
2024-01-31 08:48:37 +0200

    Delete article data using the article oop model

--- controllers/article.js
+++ controllers/article.js
@@ -1,26 +1,73 @@
 const articleDbModel = require('../models/article')
-const articleModel = new articleDbModel()
+const articleModel = new articleDbModel();
 
 class articleController {
-    constructor () {
-        this.articles = []
+    constructor() {
+        const articles = []
     }
 
-    async getAllArticles(req, res) {
+    async getAllArticles(req, res){
         const articles = await articleModel.findAll()
-        res.status(200).json({articles:articles})
+        res.status(201).json({articles: articles})
     }
 
-    async getArticleBySlug(req, res) {
+    async getArticleBySlug(req, res){
         const article = await articleModel.findOne(req.params.slug)
-        res.status(201).json({article:article})
+        res.status(201).json({article: article})
+    } 
+
+    async createdNewArticle(req, res) {
+        const newArticle = {
+            name: req.body.name,
+            slug: req.body.slug,
+            image: req.body.image,
+            body: req.body.body,
+            published: new Date().toISOString().slice(0, 19).replace('T', ' '),
+            author_id: req.body.author_id
+        }
+        const articleId = await articleModel.create(newArticle)
+        res.status(201).json({
+            message: `created article with id ${articleId}`,
+            article: {id: articleId, ...newArticle}  
+        })
+    } 
+
+    async updateArticle(req, res) {
+        const editedArticle = {
+            name: req.body.name,
+            slug: req.body.slug,
+            image: req.body.image,
+            body: req.body.body,
+            published: new Date().toISOString().slice(0, 19).replace('T', ' '),
+            author_id: req.body.author_id
+        }
+        const newArticle = await articleModel.updateOne(req.params.id, editedArticle)
+        res.status(201).json({
+            message: `created article with id ${newArticle}`,
+            article: {id: newArticle, ...editedArticle}  
+        })
+    } 
+
+    async deleteArticle(req, res) {
+        const deletedArticle = {
+            name: req.body.name,
+            image: req.body.image,
+            body: req.body.body,
+            published: new Date().toISOString().slice(0, 19).replace('T', ' '),
+            author_id: req.body.author_id
+        } 
+        const delArticle = await articleModel.deleteOne(req.params.id, deletedArticle)
+        res.status(201).json({
+            message: `deleted article with id ${delArticle}`,
+            article: {id: delArticle, ...deletedArticle}  
+        })
     }
-}
+} 
 
 module.exports = articleController
 
 // const con = require('../utils/db');
-
+// 
 // const getAllArticles = ('/', (req, res) => {
 //     let query = "SELECT * FROM article";
 //     let articles = [];
@@ -33,7 +80,7 @@ module.exports = articleController
 //         }
 //     })
 // })
-
+// 
 // const getArticleBySlug = ('/article/:slug', (req, res) => {
 //     let query = `SELECT author.name AS author, author_id AS author_id, article.image AS image, article.published AS published, article.name AS name, article.body AS body FROM article JOIN author ON article.author_id = author.id WHERE slug = "${req.params.slug}";`
 //     let article = [];
@@ -43,7 +90,7 @@ module.exports = articleController
 //         res.render('article',{ article: article });
 //     })
 // })
-
+// 
 // const getArticlesByAuthor = ('/author/:id', (req, res) => {
 //     let query = `SELECT author.name AS author, article.image AS image, article.name AS name, article.slug AS slug FROM article JOIN author ON article.author_id = author.id WHERE author_id = "${req.params.id}";`
 //     let articles = [];
@@ -54,7 +101,7 @@ module.exports = articleController
 //         res.render('author',{ author: author, articles: articles });
 //     })
 // })
-
+// 
 // module.exports = {
 //     getAllArticles,
 //     getArticleBySlug,
