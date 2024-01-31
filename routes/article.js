29563ff4af96ad5c46b4e57ceac43519ab796b64
Rasmus <rasmus.klaaser@voco.ee>
2024-01-31 09:00:55 +0200

    Delete article data using the article oop model

--- routes/article.js
+++ routes/article.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
 const router = express.Router();
-
 const articleControllerClass = require('../controllers/article');
-const articleController = new articleControllerClass();
+
+const articleController = new articleControllerClass()
 
 router.get('/', (req, res) => articleController.getAllArticles(req, res));
-router.get('/article/:slug', (req,res) => articleController.getArticleBySlug(req, res))
+router.get('/:slug', (req, res) => articleController.getArticleBySlug(req, res));
+router.post('/create', (req, res) => articleController.createdNewArticle(req, res));
+router.post('/edit/:id', (req, res) => articleController.updateArticle(req, res));
+router.delete('/delete/:id', (req, res) => articleController.deleteArticle(req, res));
 
 module.exports = router;
+
+// const express = require('express');
+// 
+// const router = express.Router();
+// 
+// const articleController = require('../controllers/article');
+// 
+// router.get('/', articleController.getAllArticles);
+// router.get('/article/:slug', articleController.getArticleBySlug);
+// router.get('/author/:id', articleController.getArticlesByAuthor);
+// 
+// module.exports = router;
