57b9ce59c560e1c1ff0c309e0fba966774ed6586
Rasmus <rasmus.klaaser@voco.ee>
2024-01-31 08:50:08 +0200

    Delete article data using the article oop model

--- models/article.js
+++ models/article.js
@@ -3,17 +3,36 @@ const BaseSQLModel = require('./base')
 class ArticleModel extends BaseSQLModel {
     constructor() {
         super('article');
-    }
+    } 
 
     async findAll() {
-        const articles = await super.findAll();
+        const articles = await super.findAll()
         return articles
-    }
+    } 
 
     async findOne(slug) {
         const article = await super.findOne('slug', slug)
         return article
-    }
-}
+    } 
 
+    async findMany(where, value) {
+        const articles = await super.findMany(where, value)
+        return articles
+    } 
+
+    async create(article) {
+        const createdArticleId = await super.create(article)
+        return createdArticleId
+    } 
+
+    async updateOne(id, data) {
+        const editedArticle = await super.update(id, data)
+        return editedArticle
+      }
+
+    async deleteOne(id) {
+        const deletedArticle = await super.delete(id)
+        return deletedArticle
+    }
+} 
 module.exports = ArticleModel;
